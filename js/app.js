c159b38c7d98c3e7928e143552a92f439e37df02
root <root@mumbleberrypi.(none)>
2015-04-23 22:35:14 +0000

    fleshed out app.js with stuff

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+, jsdom = require('jsdom')
+, request = require('request')
+, url = require('url')
+, app = module.exports = express.createServer();
