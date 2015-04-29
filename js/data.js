94d43fbd81c552fc012216db0a2458bae55a62ad
root <root@mumbleberrypi.(none)>
2015-04-29 14:16:36 +0000

    added data

---
+++ js/data.js
@@ -0,0 +1,271 @@
+{
+    "offset": 0,
+    "results": [
+        {
+            "center_value_3_numbers": 207,
+            "center_value_2_numbers/_source": "1082",
+            "center_value_1": "78",
+            "center_value_2_numbers": 1082,
+            "center_value_3": "207",
+            "center_value_2": "1082",
+            "center_value_1_numbers/_source": "78",
+            "center_value_1_numbers": 78,
+            "center_value_3_numbers/_source": "207",
+            "value": "Victories"
+        },
+        {
+            "center_value_3_numbers": 584,
+            "center_value_2_numbers/_source": "2344",
+            "center_value_1": "199",
+            "center_value_2_numbers": 2344,
+            "center_value_3": "584",
+            "center_value_2": "2344",
+            "center_value_1_numbers/_source": "199",
+            "center_value_1_numbers": 199,
+            "center_value_3_numbers/_source": "584",
+            "value": "Completed missions"
+        },
+        {
+            "center_value_3_numbers": 51,
+            "center_value_2_numbers/_source": "54",
+            "center_value_1": "44%",
+            "center_value_2_numbers": 54,
+            "center_value_3": "51%",
+            "center_value_2": "54%",
+            "center_value_1_numbers/_source": "44",
+            "center_value_1_numbers": 44,
+            "center_value_3_numbers/_source": "51",
+            "value": "Victories/battles ratio"
+        },
+        {
+            "center_value_3_numbers": 372,
+            "center_value_2_numbers/_source": "2123",
+            "center_value_1": "579",
+            "center_value_2_numbers": 2123,
+            "center_value_3": "372",
+            "center_value_2": "2123",
+            "center_value_1_numbers/_source": "579",
+            "center_value_1_numbers": 579,
+            "center_value_3_numbers/_source": "372",
+            "value": "Number of flyouts"
+        },
+        {
+            "center_value_3_numbers": 714,
+            "center_value_2_numbers/_source": "2418",
+            "center_value_1": "490",
+            "center_value_2_numbers": 2418,
+            "center_value_3": "714",
+            "center_value_2": "2418",
+            "center_value_1_numbers/_source": "490",
+            "center_value_1_numbers": 490,
+            "center_value_3_numbers/_source": "714",
+            "value": "Deaths"
+        },
+        {
+            "center_value_3_numbers": [
+                2,
+                2
+            ],
+            "center_value_2_numbers/_source": [
+                "11",
+                "12"
+            ],
+            "center_value_1": "16h 35m",
+            "center_value_2_numbers": [
+                11,
+                12
+            ],
+            "center_value_3": "2d 2h",
+            "center_value_2": "11d 12h",
+            "center_value_1_numbers/_source": [
+                "16",
+                "35"
+            ],
+            "center_value_1_numbers": [
+                16,
+                35
+            ],
+            "center_value_3_numbers/_source": [
+                "2",
+                "2"
+            ],
+            "value": "Fighter battle time"
+        },
+        {
+            "center_value_3_numbers": [
+                3,
+                53
+            ],
+            "center_value_2_numbers/_source": [
+                "1",
+                "1"
+            ],
+            "center_value_1": "3h 54m",
+            "center_value_2_numbers": [
+                1,
+                1
+            ],
+            "center_value_3": "3h 53m",
+            "center_value_2": "1d 1h",
+            "center_value_1_numbers/_source": [
+                "3",
+                "54"
+            ],
+            "center_value_1_numbers": [
+                3,
+                54
+            ],
+            "center_value_3_numbers/_source": [
+                "3",
+                "53"
+            ],
+            "value": "Bomber battle time"
+        },
+        {
+            "center_value_3_numbers": [
+                8,
+                26
+            ],
+            "center_value_2_numbers/_source": [
+                "1",
+                "12"
+            ],
+            "center_value_1": "5h 5m",
+            "center_value_2_numbers": [
+                1,
+                12
+            ],
+            "center_value_3": "8h 26m",
+            "center_value_2": "1d 12h",
+            "center_value_1_numbers/_source": [
+                "5",
+                "5"
+            ],
+            "center_value_1_numbers": [
+                5,
+                5
+            ],
+            "center_value_3_numbers/_source": [
+                "8",
+                "26"
+            ],
+            "value": "Attacker battle time"
+        },
+        {
+            "center_value_3_numbers": 187,
+            "center_value_2_numbers/_source": "1277",
+            "center_value_1": "444",
+            "center_value_2_numbers": 1277,
+            "center_value_3": "187",
+            "center_value_2": "1277",
+            "center_value_1_numbers/_source": "444",
+            "center_value_1_numbers": 444,
+            "center_value_3_numbers/_source": "187",
+            "value": "Air targets destroyed"
+        },
+        {
+            "center_value_3_numbers": 581,
+            "center_value_2_numbers/_source": "1990",
+            "center_value_1": "85",
+            "center_value_2_numbers": 1990,
+            "center_value_3": "581",
+            "center_value_2": "1990",
+            "center_value_1_numbers/_source": "85",
+            "center_value_1_numbers": 85,
+            "center_value_3_numbers/_source": "581",
+            "value": "Ground targets destroyed"
+        },
+        {
+            "center_value_3_numbers": 4801920,
+            "center_value_2_numbers/_source": "18,109,834",
+            "center_value_1": "1,350,099",
+            "center_value_2_numbers": 18109834,
+            "center_value_3": "4,801,920",
+            "center_value_2": "18,109,834",
+            "center_value_1_numbers/_source": "1,350,099",
+            "center_value_1_numbers": 1350099,
+            "center_value_3_numbers/_source": "4,801,920",
+            "value": "Lions earned"
+        },
+        {
+            "center_value_3_numbers": 532.6,
+            "center_value_2_numbers/_source": "1.5",
+            "center_value_1": "202.6K",
+            "center_value_2_numbers": 1.5,
+            "center_value_3": "532.6K",
+            "center_value_2": "1.5M",
+            "center_value_1_numbers/_source": "202.6",
+            "center_value_1_numbers": 202.6,
+            "center_value_3_numbers/_source": "532.6",
+            "value": "XP earned"
+        },
+        {
+            "center_value_3_numbers": [
+                2,
+                14
+            ],
+            "center_value_2_numbers/_source": [
+                "14",
+                "2"
+            ],
+            "center_value_1": "1d 1h",
+            "center_value_2_numbers": [
+                14,
+                2
+            ],
+            "center_value_3": "2d 14h",
+            "center_value_2": "14d 2h",
+            "center_value_1_numbers/_source": [
+                "1",
+                "1"
+            ],
+            "center_value_1_numbers": [
+                1,
+                1
+            ],
+            "center_value_3_numbers/_source": [
+                "2",
+                "14"
+            ],
+            "value": "Play time"
+        }
+    ],
+    "cookies": [
+        "identity_sid=\"50co1tvifvokf36jdcnsg27bl7\";Path=\"/\";Domain=\"warthunder.com\";Port=\"80\"",
+        "language=\"en\";Path=\"/\";Domain=\"warthunder.com\";Port=\"80\"",
+        "wt_conntrack=\"BQpUiFVA5jHBdFw+M8v5Ag==\";Path=\"/\";Domain=\"warthunder.com\";Port=\"80\""
+    ],
+    "connectorVersionGuid": "6f1dd3cd-ef4e-1b71-2143-5302cfd3e1f2",
+    "connectorGuid": "e50e62f3-c3da-4246-9033-b20578bb3f64",
+    "pageUrl": "http://warthunder.com/en/community/userinfo/?nick=Nammi_namm",
+    "outputProperties": [
+        {
+            "name": "value",
+            "type": "STRING"
+        },
+        {
+            "name": "center_value_1",
+            "type": "STRING"
+        },
+        {
+            "name": "center_value_1_numbers",
+            "type": "DOUBLE"
+        },
+        {
+            "name": "center_value_2",
+            "type": "STRING"
+        },
+        {
+            "name": "center_value_2_numbers",
+            "type": "DOUBLE"
+        },
+        {
+            "name": "center_value_3",
+            "type": "STRING"
+        },
+        {
+            "name": "center_value_3_numbers",
+            "type": "DOUBLE"
+        }
+    ]
+}
