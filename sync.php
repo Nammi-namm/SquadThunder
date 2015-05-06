fb3623039c13a8479ac653f200d0fc8125c0b377
Benedikt M. Scheving-Thorsteinsson <thenamminamm@gmail.com>
2015-05-06 13:52:40 +0000

    added comments to sync file

--- sync.php
+++ sync.php
@@ -1,22 +1,25 @@
 <?php
-
+//defina json file
 $FILENAME = "data.json";
 
+//lesir gögn úr json file.
 function readDataFile() {
   global $FILENAME;
   $contents = file_get_contents($FILENAME);
   return $contents;
 }
 
+//skrifar gogn í json file
 function writeDataFile($data) {
   global $FILENAME;
   $contents = json_encode($data);
   file_put_contents($FILENAME, $contents);
 }
 
-if($_SERVER['REQUEST_METHOD'] == "GET") {
+
+if($_SERVER['REQUEST_METHOD'] == "GET") { // keyrist þegar er fyrst loadaðtil að lesa file
   echo readDataFile();
-} elseif($_SERVER["REQUEST_METHOD"] == "POST") {
+} elseif($_SERVER["REQUEST_METHOD"] == "POST") { // eftir notandi sendir input er sent yfir i json filein.
   $data = json_decode(readDataFile());
   $data[] = json_decode($HTTP_RAW_POST_DATA); //the gögn
   writeDataFile($data);
