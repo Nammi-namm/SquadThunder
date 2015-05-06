33e3248f5a81f3cce97d9f8f88b1c73382562cca
Nammi-namm <thenamminamm@gmail.com>
2015-05-06 01:48:46 +0000

    cleanup of unused files such as database connection and un-used javascript files

--- js/scrape/scrape.js
+++
@@ -1,7 +0,0 @@
-var request = require("request");
- 
-request({
-  uri: "http://warthunder.com/en/community/userinfo/?nick=Nammi_namm",
-}, function(error, response, body) {
-  console.log(body);
-});
